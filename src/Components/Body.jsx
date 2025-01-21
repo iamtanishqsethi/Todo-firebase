285a6fb26b6085426fce28460b695be1b3477400
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-01-21 22:51:45 +0530

    firebase

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,20 +1,23 @@
 import {useEffect, useState} from "react";
-import { collection, addDoc ,getDocs} from "firebase/firestore";
+import { collection, addDoc ,getDocs,deleteDoc,doc} from "firebase/firestore";
 import {database} from "../Utils/firbaseConfig"
+import TodoItem from "./TodoItem";
 
 const Body=()=>{
     const [Todo,setTodo]=useState("");
     const [allTodo,setAllTodo]=useState([])
-    const [added,setAdded]=useState(false)
+
 
     const handleAddTodo=async (e)=>{
         e.preventDefault();
         try{
             const docRef=await addDoc(collection(database,"todos"),{
                 todo:Todo,
+                completed:false,
             })
-            console.log("Document written with ID: ", docRef.id);
-            setAdded(!added);
+
+            setTodo("")
+            fetchPost()
         }
         catch (error){
             console.error("Error adding document: ", e);
@@ -22,30 +25,41 @@ const Body=()=>{
     }
     const fetchPost = async () => {
 
-        await getDocs(collection(database, "todos"))
-            .then((querySnapshot)=>{
-                const newData = querySnapshot.docs
-                    .map((doc) => ({...doc.data(), id:doc.id }));
-                setAllTodo(newData);
-            })
+        const querySnapshot=await getDocs(collection(database, "todos"))
+        const newData = querySnapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id:doc.id
+        }));
+        setAllTodo(newData);
+
 
     }
+    const handleDelete = async (id)=>{
+        await deleteDoc(doc(database,'todos',id))
+        setAllTodo(prev => prev.filter(todo => todo.id !== id));
+
+    }
+
+
     useEffect(() => {
         fetchPost()
-    }, [added]);
+    }, []);
 
 
-    console.log(allTodo)
+    // console.log(allTodo)
     return(
         <div className={'flex flex-col justify-center items-center h-screen'}>
             <div className={'flex items-center justify-center'}>
-                <input type="text" placeholder={"enter todo "} className={'border-2 border-zinc-600'} onChange={(e) => setTodo(e.target.value)}/>
-                <button className={'bg-blue-700 p-2 m-2 text-white'} onClick={handleAddTodo}>Add Todo</button>
+                <input type="text" placeholder={"enter todo "} className={'border-2 border-zinc-600 p-1'} onChange={(e) => setTodo(e.target.value)}
+                    value={Todo}
+                />
+                <button className={'bg-blue-700 p-1 m-2 text-white rounded'} onClick={handleAddTodo}>Add Todo</button>
             </div>
 
-            <div>
+            <div className={'overflow-y-auto'}>
                 {allTodo.map((todo)=>(
-                    <p className={'border-2 p-2 m-2'}>{todo.todo}</p>
+                    <TodoItem todo={todo} key={todo.id}
+                    onDelete={handleDelete}/>
                 ))}
             </div>
         </div>
---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,57 @@
+import { doc,updateDoc} from "firebase/firestore";
+import {database} from "../Utils/firbaseConfig";
+import {useState} from "react";
+
+const TodoItem=({todo,onDelete})=>{
+    const [editable,setEditable]=useState(false);
+    const [todoMessage,setTodoMessage]=useState(todo.todo);
+    const [completed,setCompleted]=useState(todo.completed);
+
+    const handleUpdate=async ()=>{
+        await updateDoc(doc(database,'todos',todo.id),{
+            todo:todoMessage,
+        })
+        setEditable(false)
+    }
+    const handleCompleted=async ()=>{
+        await updateDoc(doc(database,'todos',todo.id),{
+            completed:!completed,
+        })
+        setCompleted(!completed);
+    }
+    return (
+        <div
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"}`}>
+            <input type="checkbox"
+                   checked={completed}
+                   onChange={handleCompleted}
+            />
+            <input type="text"
+                   className={`border outline-none w-full bg-transparent rounded-lg ${editable ? "border-black/10 px-2" : "border-transparent"} ${completed?'line-through':''}`}
+                   value={todoMessage} onChange={(e) => setTodoMessage(e.target.value)}
+                   readOnly={!editable}/>
+
+            <button
+                className='inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50'
+                onClick={() => {
+                    if (completed) {
+                        return
+                    }
+                    if (editable) {
+                        handleUpdate()
+                    } else {
+                        setEditable(prev => !prev);
+                    }
+                }}
+                disabled={completed}
+            >{editable ? "📁" : "📝"}
+            </button>
+            <button
+                className='inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0'
+                onClick={() => onDelete(todo.id)}
+            >❌
+            </button>
+        </div>
+    )
+}
+export default TodoItem
